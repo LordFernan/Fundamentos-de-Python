47fcf8044f60619bc7bef66a87838f8c90134d3d
Fernando Aguilar <feraguilar.cs@gmail.com>
2025-10-22 20:11:40 -0500

    Clase 7: Variables en Python. asignacion, nomenclatura y convenciones

---
+++ src/1.Primeros pasos/3.Variables.py
@@ -0,0 +1,28 @@
+import os 
+os.system("clear")
+print("\n ---------------------- ⚡ Variables ⚡--------------------\n")
+
+# ejemplos de variables 
+nombre = "Fernando Aguilar"
+print(f"Hola {nombre}")
+
+nombre = "Segundo Aguilar"
+print(f"Hola {nombre}")
+
+# Nota: python permite sobreescribir el valor de las variables
+
+# Distintas de formas de escribir variables que son correctas 
+mivariable = "todo en minusculas"
+mi_varaible = "con guion bajo"
+_mi_variable = "esta variable comienza con un guion bajo"
+MIVARIABLE = "nombre de la variable en mayusculas"
+mivariable2 = "variable con numeros en el nombre"
+
+# No se pueden declarar variables que empiecen con numeros, que tengan guiones intermedios o espacios.
+
+# Distintas convenciones de escribir nombres de variables
+camelcase = "empieza con minucula y el resto de las palabras emepiezan con mayuscula"
+Pascalcase = "empiezan con mayuscula y el resto de las palabras tambien empiezan con mayuscula"
+snake_case = "se separan las palabras con guiones bajos"
+
+# Es importante manetener una coherencia en el codigo. >Esto quiere decir que si elegimos una convenvion debemos de mantenerla en todo el código. 
