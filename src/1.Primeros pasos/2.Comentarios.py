75f8a7bbaf848ca3a5dabdf70e198cd695135257
Fernando Aguilar <feraguilar.cs@gmail.com>
2025-10-22 19:40:05 -0500

    Clase 6: Comentarios en python. Lineas simples y multilinea

---
+++ src/1.Primeros pasos/2.Comentarios.py
@@ -0,0 +1,12 @@
+import os
+os.system("clear")
+
+# Este es un comentario de una sola línea. 
+print("Comentarios\n") # imprimir en pantalla 
+
+"""
+Este es un comentario que abarca multiples lineas 
+Esta es la segunda linea de comentarios 
+
+"""
+
