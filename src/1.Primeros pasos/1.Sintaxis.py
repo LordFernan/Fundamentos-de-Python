8cc1cb710cb70dd8dd4e3a06ea57b3fe46ac66c1
Fernando Aguilar <feraguilar.cs@gmail.com>
2025-10-22 19:26:28 -0500

    Clase 5: Sintaxis e identacion basica en python con visual estudio code

---
+++ src/1.Primeros pasos/1.Sintaxis.py
@@ -0,0 +1,10 @@
+# Sintaxis: reglas de escritura propias del lenguaje
+
+import os
+os.system("clear")
+
+# Hay que tener cuidado con la tabulación o identación del lenguaje
+if 5>3:
+    if 4>2:
+        print("4 es mayor a 2")
+    print("5 es mayor a 3")
